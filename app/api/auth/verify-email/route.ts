557146c0d64a073d47678b93c827f7339bf51957
Anderson <avictoral@gmail.com>
2024-11-28 01:30:24 -0500

    refactor prisma schema for verify-email

--- app/api/auth/verify-email/route.ts
+++ app/api/auth/verify-email/route.ts
@@ -51,7 +51,7 @@ export async function GET(request: NextRequest) {
 
   await prisma.verificationToken.delete({
     where: {
-      identifier: verifyToken.identifier,
+      identifier: verifyToken.token,
     },
   });
 
